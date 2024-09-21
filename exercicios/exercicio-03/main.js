e3c1a8141a9c3bc7ddd63a6937029507a4f3d973
Valdir Jr <valdirrugiskijr@gmail.com>
2024-09-20 23:23:06 -0300

    inicio ex 03

---
+++ exercicios/exercicio-03/main.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(){
+    loadTheme();
+});
+
+function toggleTheme() {
+    document.body.classList.toggle("dark-mode");
+    const isDarkMode = document.body.classList.contains("dark-mode");
+    localStorage.setItem("theme", isDarkMode ? "dark" : "light");
+}
+
+function loadTheme() {
+    const theme = localStorage.getItem("theme");
+    if (theme === "dark") {
+        document.body.classList.add("dark-mode");
+    }
+}
